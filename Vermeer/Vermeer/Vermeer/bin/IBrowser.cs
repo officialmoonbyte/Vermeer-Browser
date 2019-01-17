6e9f6c2aa1d4c09d3f3b4f32228d253a1e2318ce
Alexander Ritter <braydelritter@gmail.com>
2019-01-17 08:47:21 -0600

    + IBrowser.cs

---
+++ Vermeer/Vermeer/Vermeer/bin/IBrowser.cs
@@ -0,0 +1,44 @@
+﻿using IndieGoat.MaterialFramework.Controls;
+using System.Windows.Forms;
+using Vermeer.Vermeer.Controls;
+
+namespace Vermeer.Vermeer.bin
+{
+    public class IBrowser
+    {
+
+        #region Vars
+
+        //The page where the browser control is on
+        public MaterialTabPage tabPage = new MaterialTabPage();
+
+        //Download Hander **DEVELOP IN ALPHA 1.01
+
+        //Host control
+        Panel baseControl = new Panel();
+
+        //Header of the browser
+        IBrowserHeader browserHeader;
+
+        #endregion
+
+        #region Startup
+
+
+
+        #endregion
+
+        #region MaterialTabPage
+
+
+
+        #endregion
+
+        #region Events
+
+
+
+        #endregion
+
+    }
+}
