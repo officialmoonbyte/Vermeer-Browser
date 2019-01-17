a9ac15c4aae4494c34e6f74cc21973bb22dd5e5f
corporate@moonbyte.net <corporate@moonbyte.net>
2019-01-17 05:30:24 -0600

    Pushed ILogger.cs and Vermeer.cs

---
+++ Vermeer/Vermeer/Vermeer/bin/ILogger.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vermeer.Vermeer.bin
+{
+    public class ILogger
+    {
+        public string Log;
+
+        public ILogger()
+        {
+            SetLoggingEvents(); this.AddToLog("INFO", "Finished Initializing ILogger");
+        }
+
+        #region Adding to the log
+
+        public void AddToLog(string Header, string Value)
+        {
+            string value = "[" + DateTime.Now.ToString("HH:mm") + "] " + "[" + Header.ToUpper() + "] " + Value;
+            if (Log != null) Log = Log + "\r\n" + value;
+            if (Log == null) Log = value;
+
+            Console.WriteLine(value);
+        }
+
+        public void AddWhitespace() { if (Log != null) Log += "\r\n"; Console.WriteLine(" "); }
+
+        #endregion
+
+        #region Writing Log
+
+        public void WriteLog()
+        {
+            string exeDirectory = AppDomain.CurrentDomain.BaseDirectory;
+
+            if (this.Log != null)
+            {
+                if (File.Exists(exeDirectory + "\\Log.log")) File.Delete(exeDirectory + "\\Log.log");
+                File.Create(exeDirectory + "\\Log.log").Close();
+                File.WriteAllText(exeDirectory + "\\Log.log", this.Log);
+            }
+        }
+
+        #endregion
+
+        #region Set Logging Events
+
+        public void SetLoggingEvents()
+        {
+            AppDomain.CurrentDomain.UnhandledException += ((obj, args) =>
+            {
+                UnhandledExceptionEventArgs e = args;
+
+                this.AddToLog("Current Domain Error", "Error with App Domain");
+
+                Exception ex = (Exception)e.ExceptionObject;
+
+                this.AddToLog("Current Domain", "Message : " + ex.Message);
+                this.AddToLog("Current Domain Error", "StackTrace : " + ex.StackTrace);
+                this.AddToLog("Current Domain Error", "Source : " + ex.Source);
+
+                this.WriteLog();
+            });
+            AppDomain.CurrentDomain.ProcessExit += ((obj, args) =>
+            {
+                this.WriteLog();
+            });
+        }
+
+        #endregion
+
+    }
+}
---
+++ Vermeer/Vermeer/Vermeer/bin/Vermeer.cs
@@ -0,0 +1,64 @@
+﻿using System.Windows.Forms;
+using Vermeer.Vermeer.bin;
+using Vermeer.Vermeer.pages;
+
+namespace Moonbyte.Vermeer.bin
+{
+
+    public enum VermeerPages { Mainform };
+    #pragma warning disable IDE1006 // Naming Styles
+    public static class vermeer
+    {
+
+        #region Startup
+
+        public static void ExecuteStartupEvents()
+        {
+            InitializeILogger();
+        }
+
+        #endregion
+
+        #region ILogger
+
+        public static ILogger ApplicationLogger;
+        public static void InitializeILogger() { ApplicationLogger = new ILogger(); }
+        public static class Logger
+        {
+            public static void WriteLog() { ApplicationLogger.WriteLog(); }
+            public static void SetLoggingEvents() { ApplicationLogger.SetLoggingEvents(); }
+            public static void AddWhiteSpace() { ApplicationLogger.AddWhitespace(); }
+            public static void AddToLog(string Header, string Value) { ApplicationLogger.AddToLog(Header, Value); }
+        }
+
+        #endregion
+
+        #region Open & Close forms
+
+        public static void Open(VermeerPages page) { Form pageForm = GetForm(page); ApplicationLogger.AddToLog("INFO", "Vermeer opening " + pageForm.Name); pageForm.Show(); }
+        public static void Close(VermeerPages page) { Form pageForm = GetForm(page); ApplicationLogger.AddToLog("INFO", "Vermeer closing " + pageForm.Name); GetForm(page).Close(); GetForm(page).Dispose(); }
+        public static Form GetForm(VermeerPages page)
+        {
+            foreach (Form form in Application.OpenForms) { if (form.Name == GetFormName(page)) { ApplicationLogger.AddToLog("INFO", "Got existing form page : " + form.Name); return form; } }
+            if (page == VermeerPages.Mainform) { return new mainPage(); }
+            return new mainPage();
+        }
+        public static string GetFormName(VermeerPages page)
+        {
+            if (page == VermeerPages.Mainform) { return "mainPage"; }
+            return "mainPage";
+        }
+
+        #endregion
+
+        #region Closing Application
+
+        public static void Close()
+        {
+            ApplicationLogger.AddToLog("INFO", "Application exiting through vermeer.Close()");
+            Application.Exit();
+        }
+
+        #endregion
+    }
+}
