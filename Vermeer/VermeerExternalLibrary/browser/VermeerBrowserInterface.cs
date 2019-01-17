1e3c703ad941e8adcd66c3ba846df7b4b027ec71
Alexander Ritter <braydelritter@gmail.com>
2019-01-17 08:46:51 -0600

    + Download Button + Home Button + IBrowserHeader (Initial Commit)
    NEW PROJECT - Vermeer External Library
    Vermeer External Library will have external libraries for third party developers to use to develop browsre engines
    
    + VermeerBrowserInterface.cs

---
+++ Vermeer/VermeerExternalLibrary/browser/VermeerBrowserInterface.cs
@@ -0,0 +1,38 @@
+﻿using IndieGoat.MaterialFramework.Controls;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Moonbyte.Vermeer.browser
+{
+    public interface VermeerBrowserInterface
+    {
+        /* Default / required browser events */
+        event EventHandler<DocumentTitleChange> OnTitleChange;
+        event EventHandler<DocumentURLChange> OnDocumentURLChange;
+        event EventHandler<DocumentIconChange> OnDocumentIconChange;
+
+        /* Default / required Browser Properties */
+        Control GetBrowserControl();
+        bool IsLoading();
+        bool IsBackEnabled();
+        bool IsForwardAvailable();
+        MaterialTabPage getTabPage();
+
+        /* Initialize */
+        void CreateBrowserHandle(MaterialTabPage tabPage);
+
+
+        /* Default / required Browser controlls */
+        void GoBack();
+        void GoForward();
+        void Navigate(string URL);
+    }
+
+    public class DocumentTitleChange : EventArgs
+    { public string DocumentTitle; }
+    public class DocumentURLChange : EventArgs
+    { public string DocumentURL; }
+    public class DocumentIconChange : EventArgs
+    { public Image icon; }
+}
