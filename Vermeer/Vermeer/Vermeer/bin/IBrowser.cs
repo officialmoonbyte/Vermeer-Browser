8992d12f1b0eee19b09586964d735ef800cd6d54
Alexander Ritter <braydelritter@gmail.com>
2019-01-22 18:50:45 -0600

    + Controls.ForwardButton + Controls.ForwardButton.OnPaint() + Controls.BackButton + Controls.BackButton.LoadingStates + Controls.BackButton.Status + Controls.BackButton.OnPaint(); + Controls.ReloadButton + Controls.ReloadButton.LoadingStates + Controls.ReloadButton.Status + Controls.ReloadButton.OnPaint() + Controls.SettingsButton + Controls.SettingsButton.OnPaint() + IBrowser.GenerateNewBrowserTab() + IBrowser.RenderHeaderUI(MaterialTabPage mainPage) + IBrowser.RenderBrowserUI(MaterialTabPage mainPage) *Changed IBrowser from a public object to a static library

--- Vermeer/Vermeer/Vermeer/bin/IBrowser.cs
+++ Vermeer/Vermeer/Vermeer/bin/IBrowser.cs
@@ -1,42 +1,42 @@
 ﻿using IndieGoat.MaterialFramework.Controls;
+using Moonbyte.Vermeer.bin;
 using System.Windows.Forms;
 using Vermeer.Vermeer.Controls;
 
 namespace Vermeer.Vermeer.bin
-{
-    public class IBrowser
+{ 
+    public static class IBrowser
     {
-
-        #region Vars
-
-        //The page where the browser control is on
-        public MaterialTabPage tabPage = new MaterialTabPage();
-
-        //Download Hander **DEVELOP IN ALPHA 1.01
-
-        //Host control
-        Panel baseControl = new Panel();
-
-        //Header of the browser
-        IBrowserHeader browserHeader;
-
-        #endregion
-
-        #region Startup
-
-
-
-        #endregion
 
         #region MaterialTabPage
 
+        public static void GenerateNewBrowserTab()
+        {
+            MaterialTabPage browserPage = new MaterialTabPage();
 
+            RenderBrowserUI(browserPage);
+            RenderHeaderUI(browserPage);
+
+            vermeer.baseTabControl.TabPages.Add(browserPage);
+        }
 
         #endregion
 
-        #region Events
+        #region Header
 
+        private static void RenderHeaderUI(MaterialTabPage mainPage)
+        {
 
+        }
+
+        #endregion
+
+        #region Browser Comp
+
+        private static void RenderBrowserUI(MaterialTabPage mainPage)
+        {
+
+        }
 
         #endregion
 
