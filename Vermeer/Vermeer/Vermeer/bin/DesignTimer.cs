d4b639f126bef6485fddbd2d0ebdd656ff983059
corporate@moonbyte.net <corporate@moonbyte.net>
2019-01-28 14:15:12 -0600

    +DisplayHander.cs +DesignTimer.cs

---
+++ Vermeer/Vermeer/Vermeer/bin/DesignTimer.cs
@@ -0,0 +1,36 @@
+﻿using IndieGoat.MaterialFramework.Controls;
+using Moonbyte.Vermeer.browser;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Vermeer.Vermeer.bin
+{
+
+    /// <summary>
+    /// Used to keep all GUI elemets smooth
+    /// </summary>
+    public class DesignTimer
+    {
+        public DesignTimer(MaterialTabPage mainPage, VermeerBrowserInstance instance)
+        {
+            Timer designTimer = new Timer();
+            designTimer.Tick += (obj, args) =>
+            {
+                mainPage.Text = instance.BrowserInterface.GetCurrentTitle();
+                instance.Size = new Size(mainPage.Width, mainPage.Height - 32);
+
+                foreach (Control c in mainPage.Controls)
+                {
+                    if (c is MaterialTextBox)
+                    {
+                        MaterialTextBox searchBar = (MaterialTextBox)c;
+
+                        searchBar.Text = instance.BrowserInterface.GetCurrentURL();
+                        searchBar.Width = (mainPage.Width - (32 * 4) - 64);
+                    }
+                }
+            };
+            designTimer.Start();
+        }
+    }
+}
---
+++ Vermeer/Vermeer/Vermeer/bin/Engine/DisplayHandler.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text.RegularExpressions;
+using CefSharp;
+using CefSharp.Structs;
+using IndieGoat.MaterialFramework.Controls;
+using Moonbyte.Vermeer.bin;
+
+namespace Vermeer.Vermeer.bin.Engine
+{
+    public class displayhandler : IDisplayHandler
+    {
+        public event EventHandler<IconChangedEventArgs> OnIconChanged;
+        public MaterialTabPage mainPage;
+        public class IconChangedEventArgs : EventArgs
+        { public Image icon; }
+
+        public displayhandler(MaterialTabPage mainpage) { this.mainPage = mainpage; }
+
+        public void OnAddressChanged(IWebBrowser chromiumWebBrowser, AddressChangedEventArgs addressChangedArgs)
+        {
+            
+        }
+
+        public bool OnAutoResize(IWebBrowser chromiumWebBrowser, CefSharp.IBrowser browser, CefSharp.Structs.Size newSize)
+        {
+            return false;
+        }
+
+        public bool OnConsoleMessage(IWebBrowser chromiumWebBrowser, ConsoleMessageEventArgs consoleMessageArgs)
+        {
+            return false;
+        }
+
+        public void OnFaviconUrlChange(IWebBrowser chromiumWebBrowser, CefSharp.IBrowser browser, IList<string> urls)
+        {
+            string FaviconDirectory = Environment.CurrentDirectory + "\\Website Favicons\\";
+
+            string IconName = urls[0];
+
+            foreach(char c in Path.GetInvalidFileNameChars())
+            {
+                IconName = IconName.Replace(c, ' ');
+            }
+
+            if (!Directory.Exists(FaviconDirectory)) { Directory.CreateDirectory(FaviconDirectory); vermeer.ApplicationLogger.AddToLog("INFO", "Created FaviconDirectory, Directory did not exist!"); }
+
+            if (!File.Exists(FaviconDirectory + IconName))
+            {
+                vermeer.ApplicationLogger.AddToLog("INFO", "Icon file does not exist! Downloading from " + IconName + ", " + urls[0]);
+                using (WebClient webclient = new WebClient())
+                { webclient.DownloadFile(urls[0], FaviconDirectory + IconName); }
+            }
+
+            vermeer.ApplicationLogger.AddToLog("INFO", "Invoking OnIconChanged from DisplayHandler");
+            OnIconChanged?.Invoke(this, new IconChangedEventArgs { icon = Image.FromFile(FaviconDirectory + IconName) });
+        }
+
+        public void OnFullscreenModeChange(IWebBrowser chromiumWebBrowser, CefSharp.IBrowser browser, bool fullscreen)
+        {
+            
+        }
+
+        public void OnStatusMessage(IWebBrowser chromiumWebBrowser, StatusMessageEventArgs statusMessageArgs)
+        {
+            
+        }
+
+        public void OnTitleChanged(IWebBrowser chromiumWebBrowser, TitleChangedEventArgs titleChangedArgs)
+        {
+            
+        }
+
+        public bool OnTooltipChanged(IWebBrowser chromiumWebBrowser, ref string text)
+        {
+            return false;
+        }
+
+    }
+}
