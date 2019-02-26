9414f106d2e7ead1509389a723548851c191ceac
corporate@moonbyte.net <corporate@moonbyte.net>
2019-02-26 00:19:59 -0600

    + Added NetworkManager.cs

--- Vermeer/Vermeer/Vermeer/bin/NetworkManager.cs
+++ Vermeer/Vermeer/Vermeer/bin/NetworkManager.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Vermeer.Vermeer.bin
+﻿namespace Vermeer.Vermeer.bin
 {
-    class NetworkManager
+    public class NetworkManager
     {
     }
 }
