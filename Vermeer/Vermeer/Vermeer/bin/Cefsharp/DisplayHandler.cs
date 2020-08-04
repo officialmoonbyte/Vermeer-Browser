f5bd678da3eed7391548d23da4a114bd37f6738b
alexritter@moonbyte.net <alexritter@moonbyte.net>
2020-08-04 08:08:28 -0500

    Added DisplayHandler.cs for CefSharp Added ISettingsManager.cs

---
+++ Vermeer/Vermeer/Vermeer/bin/Cefsharp/DisplayHandler.cs
@@ -0,0 +1,71 @@
+﻿using CefSharp;
+using CefSharp.Structs;
+using Moonbyte.Vermeer.bin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vermeer.Vermeer.bin.Cefsharp
+{
+    public class DisplayHandler : IDisplayHandler
+    {
+
+        #region Vars
+
+        CefBrowserInterface browserInterface;
+
+        #endregion Vars
+
+        #region Initialization
+
+        public DisplayHandler(CefBrowserInterface Interface)
+        { browserInterface = Interface; }
+
+        #endregion Initialization
+
+        public void OnAddressChanged(IWebBrowser chromiumWebBrowser, AddressChangedEventArgs addressChangedArgs)
+        {
+            
+        }
+
+        public bool OnAutoResize(IWebBrowser chromiumWebBrowser, CefSharp.IBrowser browser, Size newSize)
+        {
+            return true;
+        }
+
+        public bool OnConsoleMessage(IWebBrowser chromiumWebBrowser, ConsoleMessageEventArgs consoleMessageArgs)
+        {
+            return true;
+        }
+
+        public void OnFaviconUrlChange(IWebBrowser chromiumWebBrowser, CefSharp.IBrowser browser, IList<string> urls)
+        { browserInterface.ChangeTabIcon(urls[0]); vermeer.ApplicationLogger.AddToLog("ICON", urls[0]); }
+
+        public void OnFullscreenModeChange(IWebBrowser chromiumWebBrowser, CefSharp.IBrowser browser, bool fullscreen)
+        {
+            
+        }
+
+        public void OnLoadingProgressChange(IWebBrowser chromiumWebBrowser, CefSharp.IBrowser browser, double progress)
+        {
+            
+        }
+
+        public void OnStatusMessage(IWebBrowser chromiumWebBrowser, StatusMessageEventArgs statusMessageArgs)
+        {
+            
+        }
+
+        public void OnTitleChanged(IWebBrowser chromiumWebBrowser, TitleChangedEventArgs titleChangedArgs)
+        {
+            
+        }
+
+        public bool OnTooltipChanged(IWebBrowser chromiumWebBrowser, ref string text)
+        {
+            return true;
+        }
+    }
+}
---
+++ Vermeer/Vermeer/Vermeer/bin/ISettingsManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vermeer.Vermeer.bin
+{
+    public class ISettingsManager
+    {
+        #region Emus
+
+        public enum browserEngine { Chromium, Firefox }
+
+        #endregion Emus
+
+        #region Vars
+
+        public browserEngine BrowserEngine = browserEngine.Chromium;
+        public string ApplicationDataDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Moonbyte", "Vermeer");
+        public string CacheDataDirectory;
+
+        #endregion Vars
+
+        #region Setting Titles
+
+
+
+        #endregion Setting Titles
+
+        #region Startup
+
+        public ISettingsManager()
+        {
+            CacheDataDirectory = Path.Combine(ApplicationDataDirectory, "Cache");
+        }
+
+        #endregion Startup
+    }
+}
